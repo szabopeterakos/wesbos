2705da6e5d73c5abebb2ebc9885fe7a1bd47e67c
Szabó Péter <szabopeter@pro.local>
2019-12-15 18:55:30 +0100

    drumkit example as a typing practice basic project

---
+++ drumkit/scripts.js
@@ -0,0 +1,30 @@
+const keys = document.querySelectorAll(".keys");
+const audio = document.querySelector("audio");
+
+function playAudio() {
+    audio.currentTime = 0;
+    audio.play();
+}
+
+function hitProperKey(e) {
+    keys.forEach(k => {
+        const code = k.getAttribute("code");
+        if (code == e.code) {
+            k.classList.add("play");
+            playAudio();
+        }
+    });
+}
+
+function clearPlayingAnimation(e) {
+    if (e.propertyName !== "transform") {
+        return;
+    }
+    this.classList.remove("play");
+}
+
+window.addEventListener("keydown", hitProperKey);
+
+keys.forEach(k => {
+    k.addEventListener("transitionend", clearPlayingAnimation);
+});
