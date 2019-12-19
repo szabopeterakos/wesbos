cb8fa06285b4635d29d77a64a99e28a4fb3209c3
Szabó Péter <szabopeter@pro.local>
2019-12-19 22:30:51 +0100

    minor comments

--- slideOnScroll/scripts.js
+++ slideOnScroll/scripts.js
@@ -4,14 +4,14 @@ const images = document.querySelectorAll(".img");
 
 function handleScroll() {
     images.forEach(image => {
-        const slideInLine = window.innerHeight + window.scrollY; // half way through the image
-        const actionLine = slideInLine - image.clientHeight / 2;
+        const windowBottomLine = window.innerHeight + window.scrollY; // half way through the image
+        const actionLine = windowBottomLine - image.clientHeight / 3; // when the third of the image is shown
 
-        const imageBottom = image.offsetTop + image.clientHeight; // how far the image is the window top line
-        const isHalfShown = actionLine > image.offsetTop;
-        const isNotScrolledPast = window.scrollY <= imageBottom;
+        const imageBottom = image.offsetTop + image.clientHeight; // how far the image is the window top line + image hieght top to bottom
+        const isThirdShown = actionLine > image.offsetTop;
+        const isNotScrolledPast = window.scrollY < imageBottom;
 
-        if (isHalfShown && isNotScrolledPast) {
+        if (isThirdShown && isNotScrolledPast) {
             image.classList.add("hover");
         } else {
             image.classList.remove("hover");
