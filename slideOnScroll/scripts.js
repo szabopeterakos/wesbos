9c11443f31ee57b5e85093ee46b39e5a9d5d2e1b
Szabó Péter <szabopeter@pro.local>
2019-12-19 21:41:26 +0100

    finish slide in/out effect

--- slideOnScroll/scripts.js
+++ slideOnScroll/scripts.js
@@ -1,14 +1,22 @@
 console.log("slide in/out");
 
-const images = document.querySelectorAll('.img');
+const images = document.querySelectorAll(".img");
 
-function handleScroll(e){
-  console.log(window.scrollY);
-  console.dir(window.innerHeight);
-  images.forEach(image=>{
-    if ((window.innerHeight + window.scrollY)- image.height /2 > image.offsetTop){
-      console.log('action :)');
-    }
-  })
+function handleScroll() {
+    images.forEach(image => {
+        const slideInLine = window.innerHeight + window.scrollY; // half way through the image
+        const actionLine = slideInLine - image.clientHeight / 2;
+
+        const imageBottom = image.offsetTop + image.clientHeight; // how far the image is the window top line
+        const isHalfShown = actionLine > image.offsetTop;
+        const isNotScrolledPast = window.scrollY <= imageBottom;
+
+        if (isHalfShown && isNotScrolledPast) {
+            image.classList.add("hover");
+        } else {
+            image.classList.remove("hover");
+        }
+    });
 }
-window.addEventListener('scroll', handleScroll);
+
+window.addEventListener("scroll", handleScroll);
