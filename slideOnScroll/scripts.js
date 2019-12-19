8705f5dcf7b153ac4a0f54fada4848706b2687eb
Péter Szabó B <peter.b.szabo@ericsson.com>
2019-12-19 14:41:38 +0100

    slideOnScroll skeleton

--- slideOnScroll/scripts.js
+++ slideOnScroll/scripts.js
@@ -1,1 +1,14 @@
 console.log("slide in/out");
+
+const images = document.querySelectorAll('.img');
+
+function handleScroll(e){
+  console.log(window.scrollY);
+  console.dir(window.innerHeight);
+  images.forEach(image=>{
+    if ((window.innerHeight + window.scrollY)- image.height /2 > image.offsetTop){
+      console.log('action :)');
+    }
+  })
+}
+window.addEventListener('scroll', handleScroll);
